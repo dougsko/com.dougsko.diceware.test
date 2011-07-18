1ff378ff2fb3dee4c058cf619d62165995b71e0f
dougsko <dougtko@gmail.com>
2011-07-18 15:09:18 -0400

    init

---
+++ src/com/dougsko/diceware/test/DicewareTest.java
@@ -0,0 +1,12 @@
+package com.dougsko.diceware.test;
+
+import com.dougsko.diceware.Diceware;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class DicewareTest extends ActivityInstrumentationTestCase2<Diceware> {
+	public DicewareTest() {
+		super("com.dougsko.diceware", Diceware.class);
+	}
+
+}
